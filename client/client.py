831400b0d16a8d8bb7bbcc745366e07866317282
alex-bucevschi <bucevschi.alexandru@gmail.com>
2020-02-29 01:53:23 +0200

    finally working

--- client/client.py
+++ client/client.py
@@ -7,63 +7,84 @@ localIP     = "127.0.0.1"
 localPort   = 1997
 bufferSize  = 65535
 
-def sendFile(file, ClientSocket):
-	
-	ClientSocket.send(bytearray(file, 'utf-8'))
-	numberOfMessagesSent =  1
-	numberOfBytesSent = len(bytearray(file, 'utf-8')) 
-	f = open(file,'rb')
-	message = f.read(bufferSize)
-	while (message):
-		ClientSocket.send(message)
-		message = f.read(bufferSize)
-		numberOfMessagesSent = numberOfMessagesSent + 1
-		numberOfBytesSent = numberOfBytesSent + len(message) 
-	f.close()
-	ClientSocket.send(bytearray("[END]", 'utf-8'))
-	numberOfMessagesSent = numberOfMessagesSent + 1
-	numberOfBytesSent = numberOfBytesSent + len(bytearray("[END]", 'utf-8')) 
-	return (numberOfMessagesSent, numberOfBytesSent)
-		
+def sendFile(option, file, ClientSocket):
+    if(option == "TCP"):
+        ClientSocket.sendall(bytearray(file, 'utf-8'))
+    else:
+        ClientSocket.send(bytearray(file, 'utf-8'))
+    numberOfMessagesSent =  1
+    numberOfBytesSent = len(bytearray(file, 'utf-8')) 
+    #print(numberOfBytesSent, file)
+    
+    f = open(file,'rb')
+    message = f.read()
+    if(option == "TCP"):
+        ClientSocket.sendall(message)
+        #ClientSocket.sendall(bytearray("[END]", 'utf-8'))
+        return (2, len(message) + len(bytearray(file, 'utf-8')))
+    while (message):
+        ClientSocket.send(message)
+        numberOfMessagesSent = numberOfMessagesSent + 1
+        numberOfBytesSent = numberOfBytesSent + len(message) 
+        #print(numberOfBytesSent, message)
+        message = f.read(bufferSize)
+        
+    f.close()
+    ClientSocket.send(bytearray("[END]", 'utf-8'))
+    numberOfMessagesSent = numberOfMessagesSent + 1
+    numberOfBytesSent = numberOfBytesSent + len(bytearray("[END]", 'utf-8')) 
+    #print(numberOfBytesSent, "[END]")
+    return (numberOfMessagesSent, numberOfBytesSent)
+        
 
 def clientStreaming(option, directory):
-	option = option.upper()
-	
-	if option == "TCP":
-		sck = socket.SOCK_STREAM
-	elif option == "UDP":
-		sck = socket.SOCK_DGRAM
-	else:
-		print('[Client]Not a valid option for socket')
-		return (0, 0)
-	numberOfMessagesSent = 0
-	numberOfBytesSent = 0
-	ClientSocket = socket.socket(socket.AF_INET, sck)
-	ClientSocket.connect((localIP, localPort))
-	for root, dirs, files in os.walk(directory):
-		for name in files:
-			resp = sendFile(os.path.join(root, name), ClientSocket)
-			numberOfMessagesSent = numberOfMessagesSent + resp[0]
-			numberOfBytesSent = numberOfBytesSent + resp[1]
-	ClientSocket.send(bytearray("[STOP]", 'utf-8'))
-	numberOfMessagesSent = numberOfMessagesSent + 1
-	numberOfBytesSent = numberOfBytesSent + len(bytearray("[STOP]", 'utf-8')) 
-	return (numberOfMessagesSent, numberOfBytesSent)
+    option = option.upper()
+    
+    if option == "TCP":
+        sck = socket.SOCK_STREAM
+    elif option == "UDP":
+        sck = socket.SOCK_DGRAM
+    else:
+        print('[Client]Not a valid option for socket')
+        return (0, 0)
+    numberOfMessagesSent = 0
+    numberOfBytesSent = 0
+    ClientSocket = socket.socket(socket.AF_INET, sck)
+    ClientSocket.connect((localIP, localPort))
+    for root, dirs, files in os.walk(directory):
+        for name in files:
+            resp = sendFile(option, os.path.join(root, name), ClientSocket)
+            numberOfMessagesSent = numberOfMessagesSent + resp[0]
+            numberOfBytesSent = numberOfBytesSent + resp[1]
+            if(option == "TCP"):
+                ClientSocket.close()
+                ClientSocket = socket.socket(socket.AF_INET, sck)
+                ClientSocket.connect((localIP, localPort))
+
+    if(option == "TCP"):
+        i = 0#ClientSocket.sendall(bytearray("[STOP]", 'utf-8'))
+    else:
+        ClientSocket.send(bytearray("[STOP]", 'utf-8'))
+        numberOfMessagesSent = numberOfMessagesSent + 1
+        numberOfBytesSent = numberOfBytesSent + len(bytearray("[STOP]", 'utf-8')) 
+    #print(numberOfBytesSent, "[STOP]")
+    
+    return (numberOfMessagesSent, numberOfBytesSent)
    #data = ClientSocket.recv(BUFFER_SIZE)
    #ClientSocket.close()
   
 def main():
-	#print(len(sys.argv))
-	#print(sys.argv)
-	
-	if len(sys.argv) != 2:
-		print("usage: client.py option [TCP/UDP]")
-		return 
-	option = sys.argv[1]
-	startTime = timeit.default_timer()	
-	(numberOfMessagesSent, numberOfBytesSent) = clientStreaming(option, 'toSend')
-	stopTime = timeit.default_timer()	
-	print("Run Time: {}\nProtocol used:{}\n  number of messages sent:{} \n number of bytes sent: {}".format(stopTime- startTime, option, numberOfMessagesSent, numberOfBytesSent))
+    #print(len(sys.argv))
+    #print(sys.argv)
+    
+    if len(sys.argv) != 2:
+        print("usage: client.py option [TCP/UDP]")
+        return 
+    option = sys.argv[1]
+    startTime = timeit.default_timer()  
+    (numberOfMessagesSent, numberOfBytesSent) = clientStreaming(option, 'toSend')
+    stopTime = timeit.default_timer()   
+    print("Run Time: {}\nProtocol used:{}\n  number of messages sent:{} \n number of bytes sent: {}".format(stopTime- startTime, option, numberOfMessagesSent, numberOfBytesSent))
   
   
   
--- server/server.py
+++ server/server.py
@@ -6,71 +6,92 @@ localIP     = "127.0.0.1"
 localPort   = 1997
 bufferSize  = 65535
 
-def receiveFile(file, ServerSocket):
-	numberOfMessagesRead = 0
-	numberOfBytesRead = 0
-	file = file.decode("utf-8") 
-	directories = "".join(file.split("\\")[:-1])
-	if(directories != ''):
-		os.makedirs(directories, exist_ok=True)
-	
-	f = open(file,'wb')
-	message = ServerSocket.recv(bufferSize)
-	while(message):
-		numberOfMessagesRead = numberOfMessagesRead + 1
-		numberOfBytesRead = numberOfBytesRead + len(message)
-		if  message.decode("utf-8")  == "[END]":
-			break
-		f.write(message)
-		message = ServerSocket.recv(bufferSize)
-	f.close()
-	return (numberOfMessagesRead, numberOfBytesRead)
+def receiveFile(option, file, ServerSocket):
+    numberOfMessagesRead = 0
+    numberOfBytesRead = 0
+    file = file.decode("utf-8") 
+    directories = "".join(file.split("\\")[:-1])
+    if(directories != ''):
+        os.makedirs(directories, exist_ok=True)
+    
+    f = open(file,'wb')
+    if option == "TCP":
+        message = ServerSocket.recv(bufferSize)
+    else:
+        bytesAddressPair = ServerSocket.recvfrom(bufferSize)
+        message = ServerSocket.recv(bufferSize)
+        #address = bytesAddressPair[1]
+        
+    while(message):
+        numberOfMessagesRead = numberOfMessagesRead + 1
+        numberOfBytesRead = numberOfBytesRead + len(message)
+        #print(numberOfBytesRead, message)
+        if  message.decode("utf-8")  == "[END]":
+            break
+        f.write(message)
+        if option == "TCP":
+            message = ServerSocket.recv(bufferSize)
+        else:
+            bytesAddressPair = ServerSocket.recvfrom(bufferSize)
+            message = bytesAddressPair[0]
+            address = bytesAddressPair[1]
+        f.close()
+    return (numberOfMessagesRead, numberOfBytesRead)
 def serverStreaming(option):
-	
-	option = option.upper()
-	if option == "TCP":
-		sck = socket.SOCK_STREAM
-	elif option == "UDP":
-		sck = socket.SOCK_DGRAM
-	else:
-		print('[Server]Not a valid option for socket')
-		return (0, 0)
-	numberOfMessagesRead = 0
-	numberOfBytesRead = 0
-	ServerSocket = socket.socket(family=socket.AF_INET, type=sck)
-	ServerSocket.bind((localIP, localPort))
-	if option == "TCP":
-		ServerSocket.listen(1)
-		conn, addr = ServerSocket.accept()
-	
+    
+    option = option.upper()
+    if option == "TCP":
+        sck = socket.SOCK_STREAM
+    elif option == "UDP":
+        sck = socket.SOCK_DGRAM
+    else:
+        print('[Server] Not a valid option for socket')
+        return (0, 0)
+    numberOfMessagesRead = 0
+    numberOfBytesRead = 0
+    ServerSocket = socket.socket(family=socket.AF_INET, type=sck)
+    ServerSocket.bind((localIP, localPort))
+    if option == "TCP":
+        ServerSocket.listen(1)
+        conn, addr = ServerSocket.accept()
+    
 
-	while(True):
-		if option == "TCP":
-			message = ServerSocket.recv(bufferSize)
-		else:
-			bytesAddressPair = ServerSocket.recvfrom(bufferSize)
-			message = bytesAddressPair[0]
-			address = bytesAddressPair[1]
-		numberOfMessagesRead = numberOfMessagesRead + 1
-		numberOfBytesRead = numberOfBytesRead + len(message) 
-		if message.decode("utf-8") == "[STOP]":
-			break
-		resp = receiveFile(message, ServerSocket)	
-		numberOfMessagesRead = numberOfMessagesRead + resp[0]
-		numberOfBytesRead = numberOfBytesRead + resp[1]
-	
-	return (numberOfMessagesRead, numberOfBytesRead)
-	
+    while(True):
+        if option == "TCP":
+            message = conn.recv(bufferSize)
+        else:
+            bytesAddressPair = ServerSocket.recvfrom(bufferSize)
+            message = bytesAddressPair[0]
+            address = bytesAddressPair[1]
+        if(len(message)):
+            if option == "TCP":
+                resp = receiveFile(option, message, conn)   
+            else:
+                resp = receiveFile(option, message, ServerSocket)   
+            numberOfMessagesRead = numberOfMessagesRead + resp[0]
+            numberOfBytesRead = numberOfBytesRead + resp[1]
+        else:
+            break
+
+        numberOfMessagesRead = numberOfMessagesRead + 1
+        numberOfBytesRead = numberOfBytesRead + len(message) 
+        #print(numberOfBytesRead, message)
+        
+        if message.decode("utf-8") == "[STOP]":
+            break
+    
+    return (numberOfMessagesRead, numberOfBytesRead)
+    
 def main():
-	#print(len(sys.argv))
-	#print(sys.argv)
-	
-	if len(sys.argv) != 2:
-		print("[Server] usage: server.py option [TCP/UDP]")
-		return 
-	option = sys.argv[1]	
-	(numberOfMessagesRead, numberOfBytesRead) = serverStreaming(option)
-	print("[Server]Protocol used:{}\n  number of messages read:{} \n number of bytes reads: {}".format(option, numberOfMessagesRead, numberOfBytesRead))
+    #print(len(sys.argv))
+    #print(sys.argv)
+    
+    if len(sys.argv) != 2:
+        print("[Server] usage: server.py option [TCP/UDP]")
+        return 
+    option = sys.argv[1]    
+    (numberOfMessagesRead, numberOfBytesRead) = serverStreaming(option)
+    print("[Server] Protocol used:{}\n  number of messages read:{} \n number of bytes reads: {}".format(option, numberOfMessagesRead, numberOfBytesRead))
   
   
   
